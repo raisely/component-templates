c8f7a8037bc9bd305e9867cb724f9ad88684768a
Tom Maitland <tom@raisely.com>
2020-09-29 18:49:41 +1000

    add combined total

---
+++ combined-fundraising-total/combined-total.js
@@ -0,0 +1,47 @@
+(RaiselyComponents) =>
+	class CombinedTotal extends React.Component {
+		state = {
+			total: 0,
+		};
+		async componentDidMount() {
+			const values = this.props.getValues();
+			const results = await Promise.all(
+				values.campaigns.map(
+					async (c) =>
+						await fetch(
+							`https://api.raisely.com/v3/campaigns/${c.path}`
+						)
+				)
+			);
+
+			const campaigns = await Promise.all(results.map((r) => r.json()));
+			const total =
+				campaigns.reduce((prev, curr) => prev + curr.data.total, 0) /
+				100;
+
+			this.setState({
+				total,
+			});
+		}
+		render() {
+			const values = this.props.getValues();
+			if (!this.state.total) return null;
+			return (
+				<h1
+					style={{
+						textAlign: "center",
+						color: "white",
+					}}
+				>
+					{
+						this.state.total
+							.toLocaleString("en-US", {
+								style: "currency",
+								currency: "USD",
+							})
+							.split(".")[0]
+					}
+				</h1>
+			);
+		}
+	};
