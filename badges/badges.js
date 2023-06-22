ec0a56e8d46926f672358d3b80e173421de2d8e3
monsteronfire <monsteronfire@gmail.com>
2023-06-22 17:43:58 +0800

    Replace class component with functional component

--- badges/badges.js
+++ badges/badges.js
@@ -1,123 +1,141 @@
 /**
-  * A list of badge ids to display
-  * @field {text} badges one or more of motivation,recruit,host,lung
-  */
-
-// eslint-disable-next-line no-unused-expressions
+ * This example component renders a list of custom badges that are not default Raisely badges
+ */
 (RaiselyComponents, React) => {
 	/**
-	 * Define your badges
-	 * isActive is a function that takes the profile and the state, and
-	 * returns true or false indicating if the badge
-	 * is active for the current profile
+	 * Here, we have defined our an array of custom badge objects
+	 * These  badges will be displayed in place of the default Raisely badges
 	 */
-	const badgeConfig = [{
-		id: 'motivation',
-		name: 'Shared Motivation',
-		isActive: (p) => p && p.description,
-		// Images for active/inactive badge
-		active: 'https://raisely-images.imgix.net/switchon/uploads/badge-sharedmotivation-2-png.png',
-		inactive: 'https://raisely-images.imgix.net/switchon/uploads/badge-sharedmotivation-1-png.png',
-		description: 'Has shared their motivation for acting',
-	}, {
-		id: 'recruit',
-		name: 'Recruited a Friend',
-		isActive: (p, state) => state.recruited,
-		active: 'https://raisely-images.imgix.net/switchon/uploads/badge-switchon-2-png.png',
-		inactive: 'https://raisely-images.imgix.net/switchon/uploads/badge-switchon-1-png.png',
-		description: 'Has successfully recruited 1 friend',
-	}, {
-		id: 'host',
-		name: 'Inspiration',
-		isActive: (p) => p && p.public && p.public.hosted,
-		active: 'https://raisely-images.imgix.net/switchon/uploads/badge-inspiration-2-png.png',
-		inactive: 'https://raisely-images.imgix.net/switchon/uploads/badge-inspiration-1-png.png',
-		description: 'Has hosted a climate conversation',
-	}, {
-		id: 'lung',
-		name: 'Lung Saver',
-		isActive: (p, state) => (state.recruited >= 5),
-		active: 'https://raisely-images.imgix.net/switchon/uploads/badge-lungsaver-2-png.png',
-		inactive: 'https://raisely-images.imgix.net/switchon/uploads/badge-lungsaver-1-png.png',
-		description: 'Has switched 5 or more people!',
-	}];
-
-	/**
-	 * Once you've declared your required components, be sure to return the class
-	 * representing your final Raisely Component so it can be shown on your page.
-	 */
-	return class BadgesComponent extends React.Component {
-		state = {
-			recruited: 0,
+	const badgeConfig = [
+		{
+			// The `id` is a quick way to reference the badge
+			id: 'motivation',
+			// This is the `name` that is displayed to the user when the badge is rendered
+			name: 'Shared Motivation',
+			// This function takes in a profile and `recruited` state and returns true or false
+			// It is used to determine if the badge is active for the current profile
+			isActive: (profile) => profile && profile.description,
+			// This is the badge image that is displayed if the badge is active
+			active: 'https://raisely-images.imgix.net/switchon/uploads/badge-sharedmotivation-2-png.png',
+			// This is the badge image that is displayed if the badge is inactive
+			inactive: 'https://raisely-images.imgix.net/switchon/uploads/badge-sharedmotivation-1-png.png',
+			// This describes how the badge is activated
+			description: 'Has shared their motivation for acting',
+		},
+		{
+			id: 'recruit',
+			name: 'Recruited a Friend',
+			isActive: (profile, recruited) => recruited,
+			active: 'https://raisely-images.imgix.net/switchon/uploads/badge-switchon-2-png.png',
+			inactive: 'https://raisely-images.imgix.net/switchon/uploads/badge-switchon-1-png.png',
+			description: 'Has successfully recruited 1 friend',
+		},
+		{
+			id: 'host',
+			name: 'Inspiration',
+			isActive: (profile) => profile && profile.public && profile.public.hosted,
+			active: 'https://raisely-images.imgix.net/switchon/uploads/badge-inspiration-2-png.png',
+			inactive: 'https://raisely-images.imgix.net/switchon/uploads/badge-inspiration-1-png.png',
+			description: 'Has hosted a climate conversation',
+		},
+		{
+			id: 'lung',
+			name: 'Lung Saver',
+			isActive: (profile, recruited) => (recruited >= 5),
+			active: 'https://raisely-images.imgix.net/switchon/uploads/badge-lungsaver-2-png.png',
+			inactive: 'https://raisely-images.imgix.net/switchon/uploads/badge-lungsaver-1-png.png',
+			description: 'Has switched 5 or more people!',
 		}
+	];
+
+	// Since we have access to the main React library, we can pull out the hooks we need
+	const { useState, useEffect } = React;
+
+	// We also pull out the `api` helper from Raisely
+	const { api } = RaiselyComponents;
+
+	return (props) => {
+		// We can get the `current` state from global values
+		const { current } = useRaisely();
+
+		// Then we get the profile from the `current` state
+		const { profile } = current;
 
 		/**
-		  * Load any additional information needed for the badges
-		  */
-		componentDidMount() {
-			this.loadRecruits(this.props.global.current.profile);
+		 * You can add your own fields in your Custom Component settings
+		 * Then when you set values for these fields within the page editor, they can be accessed
+		 * here by calling props.getValues()
+		 * However, if no values are set, your fields will not be present on this values object.
+		 */
+		const values = props.getValues();
+
+		values.badges =
+			(values.badges && values.badges.split(',').map(b => b.trim())) ||
+			badgeConfig.map(b => b.id);
+
+		// Set up some state to track the number of `recruited` members
+		// Default value is set to `0`
+		const [recruited, setRecruited] = useState(0);
+
+		// Use the Raisely `api` helper to fetch the member count for the current profile
+		const loadRecruits = async () => {
+			// API Doc: https://developers.raisely.com/reference/getprofilesmembers
+			const response = await api.profiles.members.getAll({
+				id: profile.uuid,
+				query: {
+					limit: 1000,
+					status: 'ACTIVE',
+					"public.switchOnStatus": "confirmed"
+				}
+			})
+
+			// Store the `pagination` object from the response data
+			const pagination = response.data.pagination;
+
+			// If there is no pagination object, log an error
+			if (!pagination) console.log('Error fetching recruits', response.body())
+
+			// Else, set the `recruited` state to the total number of members
+			setRecruited(pagination.total);
 		}
 
-		/**
-		  * This fetches the number of profiles that are membes of this profile
-		  * which can then be used to create badges around how many people are recruited
-		  */
-		loadRecruits(profile) {
-			fetch(`https://api.raisely.com/v3/profiles/${profile.uuid}/members?public.switchOnStatus=confirmed`)
-				.then(response => response.json())
-				.then((body) => {
-					console.log(body)
-					if (!body.pagination) {
-						console.log('Error fetching recruits', body);
-					} else {
-						this.setState({ recruited: body.pagination.total });
-					}
-				});
-		}
+		useEffect(() => {
+			loadRecruits();
+		}, [])
 
-		render() {
-			const {
-				profile,
-			} = this.props.global.current;
+		// Loop over our custom badges and build some markup for each one
+		const badges = badgeConfig
+			.filter(badge => values.badges.includes(badge.id))
+			.map((badge) => {
+				// Set intial value for `isActive`
+				let isActive = false;
+				// Then re-set `isActive` based on the return value of each badge's isActive function
+				if (badge.isActive) isActive = badge.isActive(profile, recruited);
 
-			const values = this.props.getValues();
+				// Determine whether to use the `active` or `inactive` image url
+				const url = isActive ? badge.active : badge.inactive;
+				// The append a width to the url to create the final image url
+				const fullUrl = `${url}?w=200`;
 
-			// Show selected badges, or all badges
-			values.badges =
-				(values.badges && values.badges.split(',').map(b => b.trim())) ||
-				badgeConfig.map(b => b.id);
+				return (
+					<div
+						className={`badge ${isActive ? 'active' : 'inactive'}`}
+						// The `key` prop allows React to uniquely identify each
+						// element in this array. see: https://reactjs.org/docs/lists-and-keys.html
+						key={badge.name}
+					>
+						<img
+							src={fullUrl}
+							alt={badge.description} />
+						<h5>{badge.name}</h5>
+					</div>
+				);
+			});
 
-			const badges = badgeConfig
-				// Show only selected badges
-				.filter(badge => values.badges.includes(badge.id))
-				// render each selected badge
-				.map((badge) => {
-					let isActive = false;
-					if (badge.isActive) isActive = badge.isActive(profile, this.state);
-					const url = isActive ? badge.active : badge.inactive;
-
-					const fullUrl = `${url}?w=200`;
-
-					// note: we add a key prop here to allow react to uniquely identify each
-					// element in this array. see: https://reactjs.org/docs/lists-and-keys.html
-					return (
-						<div
-							className={`badge ${isActive ? 'active' : 'inactive'}`}
-						>
-							<img
-								src={fullUrl}
-								key={badge.name}
-								alt={badge.description} />
-							<h5>{badge.name}</h5>
-						</div>
-					);
-				});
-
-			return (
-				<div className="badges">
-					{badges}
-				</div>
-			);
-		}
-	};
+		return (
+			<div className="badges">
+				{badges}
+			</div>
+		)
+	}
 };
